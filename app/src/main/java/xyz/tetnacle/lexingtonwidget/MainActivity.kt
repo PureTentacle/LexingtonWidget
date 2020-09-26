944b91deb7fc32b7f8c294ac13ffd1dbee6fd42b
ReinerTentacle <526028253@qq.com>
2020-09-27 01:27:06 +0800

    version 1.0

---
+++ app/src/main/java/xyz/tetnacle/lexingtonwidget/MainActivity.kt
@@ -0,0 +1,18 @@
+package xyz.tetnacle.lexingtonwidget
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        finish()
+    }
+}
